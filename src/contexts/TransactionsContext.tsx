ef2688a4901c6dc0dde01cc4e3702885b8773690
Luis Felipe <luisrochacruzalves@gmail.com>
2024-03-07 00:10:59 -0300

    dev

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -37,7 +37,6 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
     const response = await api.get('transactions', {
       params: {
         _sort: 'createdAt',
-        _order: 'desc',
         q: query,
       },
     })
